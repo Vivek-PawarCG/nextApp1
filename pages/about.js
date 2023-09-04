24a1bc81edd315b766c85edddbc6158c6187a3a8
Vivek Pawar <105334909+Vivek-PawarCG@users.noreply.github.com>
2023-09-04 17:27:26 +0530

    first

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <div style={{backgroundColor:'gray', height:'600px'}}>
+    <p style={{fontSize:'50px', position:'absolute', top:'10%', left:'35%'}}>This is about page!!</p>
+    </div>
+  )
+}
+
